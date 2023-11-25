365641b2af97eee3ddc46b8631064e86154bca27
lmf98 <lara.frost@uni-ulm.de>
2023-11-25 15:53:54 +0100

    path relinking

--- grasppr/PR.py
+++ grasppr/PR.py
@@ -1,105 +1,62 @@
 from structure.solution import evaluate
 
-def path_relinking(initial_sol, guiding_sol, approach="greedy"):
-    
-    # get candidate values that are in guiding solution but not in initial solution
-    candidate_set = guiding_sol['sol'].difference(initial_sol['sol'])
-    # convert all to a list
-    candidate_nodes = sorted(candidate_set)
-    guiding_list = sorted(guiding_sol['sol'])
-    initial_list = sorted(initial_sol['sol'])
-    # candidate_values = initial_sol.symmetric_difference(guiding_sol)
+def path_relinking(initial_sol, guiding_sol, probLocalSearch=0):
 
-    """
-    Todo 
-    rename intermediate list as inital_list for less operations
-    rewrite as sets???
-    """
+    # remove if initial_sol, guiding_sol are sets already
+    initial_set = initial_sol['sol']
+    guiding_set = guiding_sol['sol']
 
-    #if approach=="greedy":
-    # loop over all candidate nodes
-    while len(candidate_nodes) > 1:
-        i = candidate_nodes[0]
-        # add local search here
-        best_list = initial_list
-        """
-        this only works for first time, we do not want to replace already replaced elements
-        compute set difference new?
-        """
-        best_list[0] = i
-        best_sol = evaluate(set(best_list))
-        # loop over all positions/nodes to replace in set
-        for j in len(initial_list):
-            intermediate_list = initial_list
-            intermediate_list[j] = i
-            intermediate_sol = evaluate(set(intermediate_list))
-            if intermediate_sol > best_sol:
-                best_list = intermediate_list
-        # set new inital list 
-        # add local search here later
-        initial_list = best_list
-        candidate_nodes.remove(i)
-    
-    # if only 1 candidate left define set
-    # difference best list and guiding list
-    best_set = set(best_list)
+    # save best from initial and guiding solution
+    initial_of = evaluate(initial_set)
+    guiding_of = evaluate(guiding_set)
+    best_of = min(initial_of, guiding_of)
+    if best_of == initial_of:
+        best_set = initial_set
+    else:
+        best_set = guiding_set
 
-    # exchange element with last candidate
+    intermediate_set = initial_set.copy() # copy needed?
 
+    # nodes to enter the initial set
+    nodes_enter = guiding_set.difference(initial_set)
+    # nodes from guiding set already present in the inital set
+    nodes_keep = initial_set.intersection(guiding_set)
+    # nodes in intermediate set to exchange with a entering node
+    nodes_exchange = intermediate_set.difference(nodes_keep)
 
-    return set(best_list)
+    # enter all nodes
+    while len(nodes_enter) > 0:
+        # force at least one new node to enter 
+        current_of = -1
+        for i in nodes_enter:
+            # build intermediate_set
+            intermediate_set = nodes_keep.union(nodes_exchange)
+            intermediate_set.add(i)
+            # candidates to exchange with entering node
+            for j in nodes_exchange:          
+                # check for best node to leave
+                intermediate_set.remove(j)
+                intermediate_of = evaluate(intermediate_set)
+                if  intermediate_of < current_of or current_of < 0:
+                    best_enter = i
+                    best_leave = j
+                    current_of = intermediate_of
+                    current_set = intermediate_set 
+                # rebuild nodes to exchange for next iter
+                intermediate_set.add(j)
+            intermediate_set.remove(i)
+        # remove leaving and entering nodes 
+        nodes_exchange.remove(best_leave)
+        nodes_enter.remove(best_enter)
+        
+        # check for best pr set vs best set from initial and global
+        best_pr_set = current_set
+        best_pr_of =  evaluate(best_pr_set)
+        if  best_pr_of < best_of:
+            best_of = best_pr_of
+            best_set = best_pr_set
+        
+    # add local search here, just with a probability or a counter?
+    # if probLocalSearch > 0
 
-def path_relinking_google(self, initial_solution, final_solution):
-
-        # values that differ between initial and guiding solution
-        sym_dif = initial_solution.symmetric_difference(final_solution)
-
-        cost_init_sol = self.evaluate_solution(initial_solution)
-        cost_final_sol = self.evaluate_solution(final_solution)
-
-        best_cost = min(cost_init_sol, cost_final_sol)
-
-        if cost_init_sol < cost_final_sol:
-            best_sol = initial_solution
-
-        else:
-            best_sol = final_solution
-
-        current_sol = initial_solution.copy()
-
-        best_dif_cost = 0
-        best_change_cost = best_cost
-
-        while len(sym_dif) > 0:
-
-            new_it = True
-
-            for i in sym_dif:
-
-                if i in current_sol:
-                    dif_cost = -self.subsets_cost[i]
-
-                else:
-                    dif_cost = self.subsets_cost[i]
-
-                current_sol.symmetric_difference_update({i})
-
-                if (dif_cost < best_dif_cost or new_it) and self.is_complete(current_sol):
-                    new_it = False
-
-                    best_i = i
-                    best_dif_cost = dif_cost
-
-                current_sol.symmetric_difference_update({i})
-
-            current_sol.symmetric_difference_update({best_i})
-
-            best_change_cost += best_dif_cost
-
-            if best_change_cost < best_cost:
-                best_cost = best_change_cost
-                best_sol = current_sol.copy()
-
-            sym_dif.remove(best_i)
-
-        return best_sol
+    return best_set
